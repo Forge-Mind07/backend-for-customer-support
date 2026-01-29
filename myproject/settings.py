39363fb12917eadb480da55197bfb5e31a2bf38d
Mithun <mithunjayanth2007@gmail.com>
2026-01-29 22:40:29 +0530

    Update env config and project files

--- myproject/settings.py
+++ myproject/settings.py
@@ -121,14 +121,21 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'support_system',
-        'USER': 'postgres',
-        'PASSWORD': 'Mithun@771',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
